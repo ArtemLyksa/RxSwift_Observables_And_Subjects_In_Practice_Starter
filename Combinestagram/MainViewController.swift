c393f67952fcf65c3e606bcd95123dc0ae447fd9
Artem Lyksa <artemlyksa@Artems-MacBook-Pro-3.local>
2017-09-05 15:57:48 +0300

    starter finished

--- Combinestagram/MainViewController.swift
+++ Combinestagram/MainViewController.swift
@@ -24,35 +24,86 @@ import UIKit
 import RxSwift
 
 class MainViewController: UIViewController {
-
-  @IBOutlet weak var imagePreview: UIImageView!
-  @IBOutlet weak var buttonClear: UIButton!
-  @IBOutlet weak var buttonSave: UIButton!
-  @IBOutlet weak var itemAdd: UIBarButtonItem!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-  }
-
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-  }
-
-  @IBAction func actionClear() {
-
-  }
-
-  @IBAction func actionSave() {
-  }
-
-  @IBAction func actionAdd() {
-
-  }
-
-  func showMessage(_ title: String, description: String? = nil) {
-    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
-    present(alert, animated: true, completion: nil)
-  }
+    
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var itemAdd: UIBarButtonItem!
+    
+    private let bag = DisposeBag()
+    private let images = Variable<[UIImage]>([])
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        images.asObservable()
+            .subscribe(onNext: { [weak self] photos in
+                guard let preview = self?.imagePreview else { return }
+                preview.image = UIImage.collage(images: photos, size: preview.frame.size)
+            })
+            .addDisposableTo(bag)
+        
+        images.asObservable()
+            .subscribe(onNext: { [weak self] photos in
+                self?.updateUI(photos: photos)
+            })
+            .disposed(by: bag)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+          print("resources: \(RxSwift.Resources.total)")
+    }
+    
+    @IBAction func actionClear() {
+         images.value = []
+    }
+    
+    @IBAction func actionSave() {
+        guard let image = imagePreview.image else { return }
+        
+        PhotoWriter.save(image)
+            .subscribe(onError: { [weak self] error in
+                self?.showMessage("Error", description: error.localizedDescription)
+                }, onCompleted: { [weak self] in
+                    self?.showMessage("Saved")
+                    self?.actionClear()
+            })
+            .disposed(by: bag)
+    }
+    
+    @IBAction func actionAdd() {
+//        images.value.append(UIImage(named: "IMG_1907.jpg")!)
+        let photosViewController = storyboard!.instantiateViewController(
+            withIdentifier: "PhotosViewController") as! PhotosViewController
+        
+        photosViewController.selectedPhotos
+            .subscribe(onNext: { [weak self] newImage in
+                
+                guard let images = self?.images else { return }
+                images.value.append(newImage)
+                
+                }, onDisposed: {
+                    print("completed photo selection")
+            })
+            .disposed(by: photosViewController.bag)
+        
+        navigationController!.pushViewController(photosViewController, animated:
+            true)
+        
+    }
+    
+    func showMessage(_ title: String, description: String? = nil) {
+        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func updateUI(photos: [UIImage]) {
+        buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
+        buttonClear.isEnabled = photos.count > 0
+        itemAdd.isEnabled = photos.count < 6
+        title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
+    }
 }
